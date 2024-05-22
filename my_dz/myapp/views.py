918523f4347be5030d700c88195c98f016b7e218
Suntsov Pavel <suntsovpavel@gmail.com>
2024-05-22 09:39:08 +0300

    допилка ДЗ3

--- my_dz/myapp/views.py
+++ my_dz/myapp/views.py
@@ -20,6 +20,28 @@ def about(request):
     logger.info('about page accessed')
     return render(request, "myapp/about.html")
 
+def inner_list_orders(orders: list[Order],
+                      date_limit: datetime.date):   # дата, ранее которой заказы не рассматриваются 
+    used_orders = []
+    used_products = []
+    result = []
+    while len(used_orders) < len(orders):
+        # ищем самый поздний заказ вне списка used_orders
+        order = None
+        for ord in orders:
+            if ord not in used_orders and (order == None or ord.date_ordered > order.date_ordered):
+                order = ord   
+
+        if order.date_ordered < date_limit:
+            break                             
+
+        for product in order.products.all():
+            if not product in used_products:    
+                result.append(f'name: {product.name}, date order: {order.date_ordered}')     
+                used_products.append(product)
+        used_orders.append(order)                
+    return result
+
 # ДЗ 3
 # Создайте шаблон, который выводит список заказанных клиентом товаров из всех его заказов с сортировкой по времени:
 # — за последние 7 дней (неделю)
@@ -28,46 +50,18 @@ def about(request):
 # Товары в списке не должны повторяться.
 def list_orders(request, pk_user: int):
     user = User.objects.filter(pk=pk_user).first()
-    if user is not None:    
-        # заказы пользователя:
-        orders = Order.objects.filter(customer=user) 
-        orders.reverse()
+    if user is not None:            
+        orders = Order.objects.filter(customer=user)         
 
-        data_week = []
-        exists_products: Product = []   # исключаем повторение продуктов
-        for order in orders:
-            if order.date_ordered < (datetime.date.today() - datetime.timedelta(days=7)):
-                continue
-            for product in order.products.all():
-                if not product in exists_products:
-                    data_week.append(f'name: {product.name}, date: {order.date_ordered}') 
-                    exists_products.append(product)        
-
-        # for 30 days:       
-        data_month = []
-        exists_products: Product = []   # исключаем повторение продуктов
-        for order in orders:
-            if order.date_ordered < (datetime.date.today() - datetime.timedelta(days=30)):
-                continue
-            for product in order.products.all():
-                if not product in exists_products:
-                    data_month.append(f'name: {product.name}, date: {order.date_ordered}') 
-                    exists_products.append(product)  
-
-        # for 365 days                                
-        data_year = []
-        exists_products: Product = []   # исключаем повторение продуктов
-        for order in orders:
-            if order.date_ordered < (datetime.date.today() - datetime.timedelta(days=365)):
-                continue
-            for product in order.products.all():
-                if not product in exists_products:
-                    data_year.append(f'name: {product.name}, date: {order.date_ordered}') 
-                    exists_products.append(product)           
-
-        context = {'data_week': data_week,
-                   'data_month': data_month,
-                   'data_year': data_year }        
-    # else:
-    #     context = {'content': 'Пользователь не найден'}
+        context = {'incorrect_user': False,
+                    'data_week': inner_list_orders(orders, (datetime.date.today() - datetime.timedelta(days=7))),
+                   'data_month': inner_list_orders(orders, (datetime.date.today() - datetime.timedelta(days=30))),
+                   'data_year': inner_list_orders(orders, (datetime.date.today() - datetime.timedelta(days=365))), 
+                   }        
+    else:
+        context = {'incorrect_user': True}
     return render (request, 'myapp/list_orders.html', context)
+
+# ДЗ 4
+# Доработаем задачу про клиентов, заказы и товары из прошлого семинара.
+# Создайте форму для редактирования товаров в базе данных.
